1e27b694d2cca7cad8ebd2b5f7ee4e941141660e
Spectre-NSX <Spectre-NSX@users.noreply.github.com>
2020-03-31 19:59:17 +0100

    FINAL 2

--- js/myscript.js
+++ js/myscript.js
@@ -1,45 +1,7 @@
 var data = [4, 8, 15, 16, 23, 42];
 
-// // Div Chart:
-// var x = d3.scaleLinear()
-//     .domain([0, d3.max(data)])
-//     . range([0, 430]);
 
-// d3.select(".chart ")
-//     .selectAll("div")
-//     .data(data)
-//     .enter().append("div")
-//     .style("width", function(d) { return x(d) + "px"; }) 
-//     .text ( function (d) { return d; }) ;
-
-// SVG Chart
-// var width = 420 ,
-// barHeight = 20;
-
-// var x = d3.scaleLinear ()
-//     .domain([0, d3.max(data)])
-//     .range([0, width]) ;
-
-// var chart = d3.select (".chart ")
-//     .attr("width", width)
-//     .attr("height", barHeight * data.length);
-
-// var bar = chart.selectAll ("g")
-//     .data(data)
-//     .enter().append("g")
-//     .attr("transform", function(d, i) { return "translate(0," + i * barHeight +")";});
-
-// bar.append("rect")
-//     .attr("width", x)
-//     .attr("height", barHeight - 1);
-
-// bar.append("text")
-//     .attr("x", function(d) { return x (d) - 3;})
-//     .attr("y", barHeight / 2)
-//     .attr("dy", ".35em")
-//     .text(function(d) {return d; }) ;
-
-
+// Code for tooltip from: https://bl.ocks.org/alandunning/274bf248fd0f362d64674920e85c1eb7
 
 
 
